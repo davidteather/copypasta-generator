0750002afaa7e926650d2448cb0dda3f2fc35e04
davidteather <davidteather@users.noreply.github.com>
2020-08-20 19:28:17 -0500

    More data scraping

--- collect_pasta.py
+++ collect_pasta.py
@@ -14,15 +14,23 @@ reddit = praw.Reddit(client_id=settings['client_id'],
 
 sorting_type = "top"
 
-if sorting_type == "top":
-    pastas = reddit.subreddit("copypasta").top(limit=10000)
-elif sorting_type == "new":
-    pastas = reddit.subreddit("copypasta").new(limit=10000)
+pastas = reddit.subreddit("copypasta").top(limit=10000)
+pastas_new = reddit.subreddit("copypasta").new(limit=10000)
+pastas_hot = reddit.subreddit("copypasta").hot(limit=10000)
 
 text = ""
 for pasta in pastas:
     if pasta.is_self:
         text += pasta.selftext
+
+for pasta in pastas_new:
+    if pasta.is_self:
+        text += pasta.selftext
+
+for pasta in pastas_hot:
+    if pasta.is_self:
+        text += pasta.selftext
+
 
 with open("data.txt", 'w+', encoding='utf-8') as output:
     output.write(text)
