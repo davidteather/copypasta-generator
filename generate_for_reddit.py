fe241b675ee78ec713cdfc6e0a2935e25cfcb26a
davidteather <davidteather@users.noreply.github.com>
2020-08-21 17:24:47 -0500

    Added posting to reddit

---
+++ generate_for_reddit.py
@@ -0,0 +1,143 @@
+import praw
+import json
+
+
+import tensorflow as tf
+
+import numpy as np
+import os
+import json
+import time
+# Loads Settings
+with open("reddit_details.json", 'r') as i:
+    settings = json.loads(i.read())
+
+# Creates Reddit object to read submissions
+reddit = praw.Reddit(client_id=settings['client_id'],
+                     client_secret=settings['client_secret'],
+                     user_agent=settings['user_agent'],
+                     username=settings['username'],
+                     password=settings['password'])
+
+with open("settings.json", 'r') as i:
+  j = json.loads(i.read())
+
+  embedding_dim = j['embedding_dim']
+  rnn_units = j['rnn_units']
+  path_to_file = j['path_to_file']
+  BATCH_SIZE = j['BATCH_SIZE']
+  BUFFER_SIZE = j['BUFFER_SIZE']
+  seq_length = j['seq_length']
+  EPOCHS = j['EPOCHS']
+
+
+#
+#    DON'T TOUCH
+#
+
+# Read, then decode for py2 compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+
+vocab = sorted(set(text))
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+
+examples_per_epoch = len(text)//(seq_length+1)
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk):
+  input_text = chunk[:-1]
+  target_text = chunk[1:]
+  return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(
+    vocab_size = len(vocab),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units,
+    batch_size=BATCH_SIZE)
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+
+checkpoint_dir = './training_checkpoints'
+tf.train.latest_checkpoint(checkpoint_dir)
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 1000
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+    predictions = model(input_eval)
+    # remove the batch dimension
+    predictions = tf.squeeze(predictions, 0)
+
+    # using a categorical distribution to predict the character returned by the model
+    predictions = predictions / temperature
+    predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+    # We pass the predicted character as the next input to the model
+    # along with the previous hidden state
+    input_eval = tf.expand_dims([predicted_id], 0)
+
+    text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+gen_text = generate_text(model, start_string=u"Somebody once told me: ")
+gen_text = gen_text.replace('Somebody once told me:', "")
+
+print(gen_text)
+reddit.subreddit("copypasta").submit("Somebody once told me", selftext=gen_text)
