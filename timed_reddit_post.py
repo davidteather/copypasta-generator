b5bfec2b194c7352468eb0dcf8b84072f9185f21
davidteather <davidteather@users.noreply.github.com>
2020-08-21 17:50:05 -0500

    Added a few starters

--- timed_reddit_post.py
+++ timed_reddit_post.py
@@ -1,10 +1,16 @@
 from generate_for_reddit import *
 import praw
+import random
 import time
+
+starting_phrase = ['Somebody once told me', 'My parents disowned me, because', 
+                    'I asked someone out and they responded with', "You're welcome", 'I spent years discovering this', 
+                    'Some guy on discord just sent this to me', 'My friend thinks this is good copypasta', 
+                    'edgy teenagers be like']
 
 while True:
     try:
-        post_pasta("My parents left me, because ")
+        post_pasta(random.choice(starting_phrase))
     except praw.exceptions.RedditAPIException as e:
         try:
             if "minutes" in e.items[0].message.split("try again in ")[1]:
@@ -16,7 +22,7 @@ while True:
             
             time_delay = e.items[0].message.split("try again in ")[1].split(" minutes.")[0]
             print("sleeping {} {}".format(time_delay, phrase))
-            time.sleep(int(time_delay)*60)
+            time.sleep(int(time_delay)*multiplier)
         except Exception as e:
             print(e)
             print("No specific time catch")
@@ -25,4 +31,5 @@ while True:
         break
 
     except OSError:
+        print("???")
         continue
