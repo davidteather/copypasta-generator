cd5a3c82d152359b951b369db1357e44622561f1
davidteather <davidteather@users.noreply.github.com>
2020-08-20 19:20:46 -0500

    Fix ML

--- collect_pasta.py
+++ collect_pasta.py
@@ -1,1 +1,28 @@
 import praw
+import json
+
+# Loads Settings
+with open("reddit_details.json", 'r') as i:
+    settings = json.loads(i.read())
+
+# Creates Reddit object to read submissions
+reddit = praw.Reddit(client_id=settings['client_id'],
+                     client_secret=settings['client_secret'],
+                     user_agent=settings['user_agent'],
+                     username=settings['username'],
+                     password=settings['password'])
+
+sorting_type = "top"
+
+if sorting_type == "top":
+    pastas = reddit.subreddit("copypasta").top(limit=10000)
+elif sorting_type == "new":
+    pastas = reddit.subreddit("copypasta").new(limit=10000)
+
+text = ""
+for pasta in pastas:
+    if pasta.is_self:
+        text += pasta.selftext
+
+with open("data.txt", 'w+', encoding='utf-8') as output:
+    output.write(text)
--- train.py
+++ train.py
@@ -3,8 +3,48 @@ import tensorflow as tf
 import numpy as np
 import os
 import time
+import json
 
-path_to_file = "data.txt"
+#
+#             CONFIGURATION VARIABLES
+#
+
+# The embedding dimension
+#embedding_dim = 256
+
+# Number of RNN units
+#rnn_units = 1024
+
+#path_to_file = "data.txt"
+
+# Batch size
+#BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+#BUFFER_SIZE = 10000
+
+# The maximum length sentence we want for a single input in characters
+#seq_length = 100
+
+
+with open("settings.json", 'r') as i:
+  j = json.loads(i.read())
+
+  embedding_dim = j['embedding_dim']
+  rnn_units = j['rnn_units']
+  path_to_file = j['path_to_file']
+  BATCH_SIZE = j['BATCH_SIZE']
+  BUFFER_SIZE = j['BUFFER_SIZE']
+  seq_length = j['seq_length']
+  EPOCHS = j['EPOCHS']
+
+
+#
+#    DON'T TOUCH
+#
 
 # Read, then decode for py2 compat.
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
@@ -17,8 +57,7 @@ idx2char = np.array(vocab)
 
 text_as_int = np.array([char2idx[c] for c in text])
 
-# The maximum length sentence we want for a single input in characters
-seq_length = 100
+
 examples_per_epoch = len(text)//(seq_length+1)
 
 # Create training examples / targets
@@ -33,25 +72,12 @@ def split_input_target(chunk):
 
 dataset = sequences.map(split_input_target)
 
-# Batch size
-BATCH_SIZE = 64
-
-# Buffer size to shuffle the dataset
-# (TF data is designed to work with possibly infinite sequences,
-# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
-# it maintains a buffer in which it shuffles elements).
-BUFFER_SIZE = 10000
-
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
 # Length of the vocabulary in chars
 vocab_size = len(vocab)
 
-# The embedding dimension
-embedding_dim = 256
 
-# Number of RNN units
-rnn_units = 1024
 
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
@@ -65,12 +91,15 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   ])
   return model
 
+model = build_model(
+    vocab_size = len(vocab),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units,
+    batch_size=BATCH_SIZE)
+
 def loss(labels, logits):
   return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
-example_batch_loss  = loss(target_example_batch, example_batch_predictions)
-print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
-print("scalar_loss:      ", example_batch_loss.numpy().mean())
 
 model.compile(optimizer='adam', loss=loss)
  
@@ -85,7 +114,5 @@ checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_prefix,
     save_weights_only=True)
-
-EPOCHS=10
 
 history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
